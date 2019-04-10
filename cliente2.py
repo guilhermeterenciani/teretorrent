4a6d9a5e397f3b0a86d01428e29ee0f219d53b38
tere <guilhermeterenciani@gmail.com>
2019-04-09 22:56:19 -0400

    Servidor e Cliente UDP
    Modificação do Código apresentado pela professora Hana.

---
+++ cliente2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# coding: utf-8
+
+from socket import *
+
+def main():
+    SERVER_NAME = 'localhost'
+    SERVER_PORT = 12000
+
+    client_socket = socket(AF_INET, SOCK_DGRAM)
+    try:
+
+        while True:
+
+            message = "tere2"
+            client_socket.sendto(message, (SERVER_NAME, SERVER_PORT))
+
+            modifiedMessage, serverAddress = client_socket.recvfrom(2048)
+            print 'Mensagem recebida:', modifiedMessage
+    except KeyboardInterrupt:
+        client_socket.close()
+
+if __name__ == '__main__':
+    main()
---
+++ udpserver.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# coding: utf-8
+
+import time;
+from socket import *
+
+class Transmissor(object):
+    """docstring for Transmissor"""
+    def __init__(self,port):
+        super(Transmissor, self).__init__()
+        #self.arg = arg
+        self.server_socket = socket(AF_INET, SOCK_DGRAM);
+        self.SERVER_PORT = port
+        self.server_socket.bind(('', self.SERVER_PORT))
+
+    def enviaArquivo(self):
+        #mensagem = "Tere"
+        try:
+            while True:
+                message, client_address = self.server_socket.recvfrom(2048)
+                answer = message.upper()
+                #print("Vou dormir por 5 segundos");
+                #time.sleep(5);
+                #print("Parei de dormir");
+
+                self.server_socket.sendto(answer, client_address)
+
+                print 'Pacote recebido de', client_address
+
+        except KeyboardInterrupt:
+            self.server_socket.close()
+
+            print 'Servidor encerrado.'
+
+
+def main():
+    transmissor = Transmissor(12000);
+    transmissor.enviaArquivo();
+
+    
+
+
+if __name__ == '__main__':
+    main()
