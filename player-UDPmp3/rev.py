7d3fef1a827b2198394285c097d5196f2340a385
ternciani <guilhermeterenciani@gmail.com>
2019-04-30 09:30:11 -0400

    Transmissor com problemas do recorte
    Ainda não consegui fixar o problema de quando o transmissor tem que quebrar o arquivo em fatias de 320bytes. Para arquivos grandes está funcionando. Quando o arquivo é de 320bytes dá problema ao executar no receptor.

--- player-UDPmp3/rev.py
+++ player-UDPmp3/rev.py
@@ -3,15 +3,40 @@
 from socket import *
 from pydub import AudioSegment
 from pydub.playback import play
+import pygame
+from pygame.locals import *
 import io
 def main():
     sock = socket(AF_INET, SOCK_DGRAM)
     try:
-        sock.sendto("luffy.mp3",("localhost",12000))
+        sock.sendto("luffy2.mp3",("localhost",12000))
+        #AudioSegment.converter = which("ffmpeg")
+        i = 0;
+        datarecv = '';
         while 1:  
-            data, server = (sock.recvfrom(65500))
-            song = AudioSegment.from_file(io.BytesIO(data), format="mp3")
-            play(song) #with pydub
+            data, server = (sock.recvfrom(3200))
+            #print data
+            datarecv += data
+            #song.set_frame_rate(3200)
+            with open('receber.mp3', 'wb') as infile:
+                infile.write(data)
+
+            i=i+1;
+            datarecv += data
+            if(i%1==0):
+                #print("Datarecv"+datarecv+'\n')
+                i=0
+                song = AudioSegment.from_mp3('receber.mp3')
+
+                '''
+                pygame.init()
+                musica = pygame.mixer.Sound(datarecv)
+                musica.set_volume(1.0)
+                musica.play(-1)
+                musica.stop()
+                '''
+                datarecv = ''
+                play(song);
     except KeyboardInterrupt:
         print("Finalizando programa");
 
--- player-UDPmp3/testeaudio.py
+++ player-UDPmp3/testeaudio.py
@@ -3,6 +3,7 @@
 import wave
 from socket import *
 import time
+from pydub import AudioSegment
 
 def main():
     
@@ -12,12 +13,35 @@ def main():
     try:
         data, cliente = sock.recvfrom(1200);
         print("Recebido requisição do cliente "+cliente[0]+" Do dado= "+data)
+        '''
         with open('sender/'+data, 'rb') as infile:
-            d = infile.read(65500)
+            d = infile.read(320)
             while d :
-                time.sleep(1)
+                time.sleep(0.1)
                 sent = sock.sendto(d,cliente)
-                d = infile.read(65500)
+                d = infile.read(320)
+        '''
+        sound = AudioSegment.from_mp3('sender/'+data)
+
+        # len() and slicing are in milliseconds
+        end = len(sound);
+        pct = 0;
+        recorte = sound[0:1]
+        print len(recorte)
+        recorte.export('new.mp3', format='mp3')
+
+
+        with open('new.mp3', 'rb') as infile:
+            d = infile.read(320)
+            while d :
+                time.sleep(0.1)
+                sent = sock.sendto(d,cliente)
+                d = infile.read(320)
+        
+        #sent = sock.sendto(d.raw_data,cliente)
+
+        # Concatenation is just adding
+        #second_half_3_times = second_half + second_half + second_half
     except KeyboardInterrupt:
         print("Finalizando programa");
 
