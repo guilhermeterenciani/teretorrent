93d6c4bc0b201956777424e7d28acc1679698117
ternciani <guilhermeterenciani@gmail.com>
2019-05-10 13:54:07 -0400

    Testes em particionar o arquivo em 320bytes.

--- player-UDPmp3/rev.py
+++ player-UDPmp3/rev.py
@@ -3,40 +3,20 @@
 from socket import *
 from pydub import AudioSegment
 from pydub.playback import play
-import pygame
-from pygame.locals import *
+import pyaudio
+
+p = pyaudio.PyAudio()
 import io
 def main():
     sock = socket(AF_INET, SOCK_DGRAM)
+    stream = p.open(format=8,channels=2,rate=44100,output=True)
     try:
-        sock.sendto("luffy2.mp3",("localhost",12000))
-        #AudioSegment.converter = which("ffmpeg")
-        i = 0;
-        datarecv = '';
+        sock.sendto("NIN.mp3".encode('utf-8'),("localhost",12000))
         while 1:  
-            data, server = (sock.recvfrom(3200))
-            #print data
-            datarecv += data
-            #song.set_frame_rate(3200)
-            with open('receber.mp3', 'wb') as infile:
-                infile.write(data)
+            data, server = (sock.recvfrom(320))
+            stream.write(data);
 
-            i=i+1;
-            datarecv += data
-            if(i%1==0):
-                #print("Datarecv"+datarecv+'\n')
-                i=0
-                song = AudioSegment.from_mp3('receber.mp3')
-
-                '''
-                pygame.init()
-                musica = pygame.mixer.Sound(datarecv)
-                musica.set_volume(1.0)
-                musica.play(-1)
-                musica.stop()
-                '''
-                datarecv = ''
-                play(song);
+            
     except KeyboardInterrupt:
         print("Finalizando programa");
 
--- player-UDPmp3/testeaudio.py
+++ player-UDPmp3/testeaudio.py
@@ -4,6 +4,8 @@ import wave
 from socket import *
 import time
 from pydub import AudioSegment
+import pyaudio
+from pydub.playback import play
 
 def main():
     
@@ -11,37 +13,27 @@ def main():
     sock = socket(AF_INET, SOCK_DGRAM)
     sock.bind(('', 12000))
     try:
-        data, cliente = sock.recvfrom(1200);
-        print("Recebido requisição do cliente "+cliente[0]+" Do dado= "+data)
-        '''
-        with open('sender/'+data, 'rb') as infile:
-            d = infile.read(320)
-            while d :
-                time.sleep(0.1)
-                sent = sock.sendto(d,cliente)
-                d = infile.read(320)
-        '''
-        sound = AudioSegment.from_mp3('sender/'+data)
-
-        # len() and slicing are in milliseconds
-        end = len(sound);
-        pct = 0;
-        recorte = sound[0:1]
-        print len(recorte)
-        recorte.export('new.mp3', format='mp3')
-
-
-        with open('new.mp3', 'rb') as infile:
-            d = infile.read(320)
-            while d :
-                time.sleep(0.1)
-                sent = sock.sendto(d,cliente)
-                d = infile.read(320)
+        #data, cliente = sock.recvfrom(1200);
+        #print("Recebido requisição do cliente "+cliente[0]+" Do dado= "+data.decode())
         
-        #sent = sock.sendto(d.raw_data,cliente)
+        #Abre o arquivo no formato mp3 ps: só aceita mp3
+        #print('sender/'+data.decode()+'\n')
+        song = AudioSegment.from_file("sender/NIN.mp3",format="mp3")#(sender/'+data.decode(), format="mp3")
+        p = pyaudio.PyAudio()
 
-        # Concatenation is just adding
-        #second_half_3_times = second_half + second_half + second_half
+        stream = p.open(format=p.get_format_from_width(song.sample_width),
+                    channels=song.channels,
+                    rate=song.frame_rate,
+                    output=True)
+        print("Audio info: format=%d channels= %d rate= %d"
+            %(p.get_format_from_width(song.sample_width),song.channels,song.frame_rate))
+        fileSize = len(song.raw_data)
+        chunkSize = 320
+        #stream.write(song.raw_data[6000000:11150000])
+        for piece in range(0, fileSize, chunkSize):
+            print(len(song.raw_data[piece:piece+chunkSize]))
+            stream.write(song.raw_data[piece:piece+chunkSize])
+
     except KeyboardInterrupt:
         print("Finalizando programa");
 
